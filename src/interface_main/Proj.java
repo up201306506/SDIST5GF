740778616123bd51a5d86f45f0f678a29b4af53d
PedroJoaoPereira <pedro.pereira.95@hotmail.com>
2016-03-07 12:08:47 +0000

    Iniciamos sockets :+1:

--- src/interface_main/Proj.java
+++ src/interface_main/Proj.java
@@ -1,8 +1,17 @@
 package interface_main;
 
+import java.io.IOException;
+
+import network_communications.MC_Socket;
+
 public class Proj {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+	public static void main(String[] args) throws NumberFormatException, IOException, InterruptedException {
+		MC_Socket p1 = new MC_Socket(args[0], Integer.parseInt(args[1]));
+		
+		while(true){
+			System.out.println(p1.receive());
+			Thread.sleep(1000);
+		}
 	}
 }
--- src/network_communications/M_Socket.java
+++ src/network_communications/M_Socket.java
@@ -2,26 +2,67 @@ package network_communications;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public abstract class M_Socket {
 	
 	InetAddress multicastAddress;
-	byte[] bufferData;
-	DatagramPacket packet;
-	MulticastSocket socket;
+	static byte[] bufferData;
 	int port;
 	
-	public M_Socket(String address, int p) throws IOException{
+	Queue<String> messageQueue;
+	
+	MulticastSocket multicastSocket;
+	DatagramSocket datagramSocket;
+	
+	DatagramPacket packet;
+	
+	Thread multicastReceiveThread;
+	
+	public M_Socket(String address, int p, int bufLength) throws IOException{
+		
 		multicastAddress = InetAddress.getByName(address);
+		bufferData = new byte[bufLength];
 		port = p;
-		socket = new MulticastSocket(port);
-		socket.joinGroup(multicastAddress);
+		
+		messageQueue = new LinkedList<String>();
+		
+		multicastSocket = new MulticastSocket(port);
+		multicastSocket.joinGroup(multicastAddress);
+		
+		datagramSocket = new DatagramSocket();
+		
+		packet = new DatagramPacket(bufferData, bufferData.length);
+		
+		multicastReceiveThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						multicastSocket.receive(packet);
+						String tmp = new String(packet.getData(), 0, packet.getLength());
+						messageQueue.add(tmp);
+					} catch (IOException e) {
+						e.printStackTrace();
+					} 
+				}
+			}
+		});
+		multicastReceiveThread.start();
 	}
 	
 	public abstract Boolean send();
-	public abstract String receive();
 	
-	
+	public String receive() {
+		String tmp = messageQueue.peek();
+		
+		if(tmp == null) tmp = "NADA";
+		else messageQueue.remove();
+		
+		return tmp;
+	};
 }
