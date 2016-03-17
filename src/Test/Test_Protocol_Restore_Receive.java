4c71933754d9cf95a3f6201b18ad542cc15101f1
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2016-03-17 16:10:11 +0000

    :tropical_drink: Random restore

---
+++ src/Test/Test_Protocol_Restore_Receive.java
@@ -0,0 +1,27 @@
+package Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import file_utils.ChunkKey;
+import file_utils.FileManager;
+import network_communications.M_Socket;
+import protocol_communications.Backup_Protocol;
+import protocol_communications.Restore_Protocol;
+
+public class Test_Protocol_Restore_Receive {
+	
+	public static void main(String[] args) {
+		
+		FileManager fm = new FileManager();
+		
+		Map<ChunkKey, Integer> chunkStored = new HashMap<>();
+		
+		M_Socket mc = new M_Socket("224.225.226.230", 12345);
+		M_Socket mdr = new M_Socket("224.225.226.232", 12346);
+		
+		Restore_Protocol rp = new Restore_Protocol(fm, chunkStored, mc, mdr);
+		
+		
+	}
+}
--- src/protocol_communications/Restore_Protocol.java
+++ src/protocol_communications/Restore_Protocol.java
@@ -1,9 +1,17 @@
 package protocol_communications;
 
+import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import file_utils.ChunkKey;
 import file_utils.FileManager;
+import file_utils.ProtocolEnum;
 import network_communications.M_Socket;
 
 public class Restore_Protocol extends Protocol {
@@ -26,15 +34,61 @@ public class Restore_Protocol extends Protocol {
 		int numOfTries = 1;
 		int waitInterval = _INITIAL_REPLY_WAIT_TIME;
 		
+		final boolean getchunkComplete = false;
+		
+		
+		while(numOfTries <= _MAX_NUMBER_OF_RETRIES && !getchunkComplete){
+			
+			mc.send(_HEAD + " " + version + " " + senderId + " " + fileId + " " + chunkNum + " " + _CRLF + _CRLF);
+			
+/*
+			ExecutorService executor = Executors.newSingleThreadExecutor();
+			try {
+				executor.submit(new Runnable() {
+					
+					@Override
+					public void run() {
+						
+						while(true){
+							String data = mc.receive(ProtocolEnum.STORED);
+							if(data != null){
+								
+								System.out.println("stored");
+								
+								// CHUNK  Part
+								int blankSpaceIndex = data.indexOf(" ");
+								String holder = data.substring(0, blankSpaceIndex);								
+								if(holder.equals(_REPLY_HEAD))
+								{
+									//getchunkComplete = true;
+									return;
+								}
+							}
+						}
+					}
+				}).get(waitInterval, TimeUnit.SECONDS);
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			} catch (TimeoutException e) {
+				System.out.println("TIMEOUT");
+				numOfTries++;
+				waitInterval = waitInterval * 2;
+			}
+			executor.shutdown();
+*/	
+			
+		}
+		
+		
 		
 		
 		
 		return false;
 	}
 	
-	public Boolean receiveChunkResponse(){
-		return false;
-	}
 	
 	
 	//Receiving Chunk Request    [ ] --> Chunk ---> [X]
