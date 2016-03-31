369de5ac98da480979c5a8e4a8d6cf4d5e0638b7
PedroJoaoPereira <pedro.pereira.95@hotmail.com>
2016-03-31 14:16:02 +0100

    Para testar noutro pc, reclaim

---
+++ src/Test/Test_Protocol_Reclaim_Receive.java
@@ -0,0 +1,44 @@
+package Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+import file_utils.FileManager;
+import file_utils.ReplicationValue;
+import file_utils.StoreChunkKey;
+import network_communications.M_Socket;
+import protocol_communications.Backup_Protocol;
+import protocol_communications.Deletion_Protocol;
+import protocol_communications.Reclaim_Protocol;
+
+public class Test_Protocol_Reclaim_Receive {
+
+	public static void main(String[] args) {
+
+		String thisPeerId = "este_pc";
+
+		FileManager fm = new FileManager(thisPeerId);
+
+		Map<String, String> fileNames = fm.readFileIdToName();
+		Map<StoreChunkKey, ReplicationValue> chunkStored = fm.readStoreChunkReplicationRegisters();
+
+		M_Socket mc = new M_Socket("224.224.224.224", 15000);
+		M_Socket mdb = new M_Socket("224.224.224.225", 15001);
+
+		Backup_Protocol bp = new Backup_Protocol(fm, fileNames, chunkStored, mc, mdb, thisPeerId);
+		Reclaim_Protocol rp = new Reclaim_Protocol(fm, fileNames, chunkStored, mc, thisPeerId, bp);
+
+		System.out.println("Started receiving...");
+		System.out.println("< press any key to stop executing >");
+
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Stopping!");
+
+		System.exit(0);
+	}
+}
---
+++ src/Test/Test_Protocol_Reclaim_Send.java
@@ -0,0 +1,39 @@
+package Test;
+
+import java.util.Map;
+
+import file_utils.FileManager;
+import file_utils.ReplicationValue;
+import file_utils.StoreChunkKey;
+import network_communications.M_Socket;
+import protocol_communications.Backup_Protocol;
+import protocol_communications.Deletion_Protocol;
+import protocol_communications.Reclaim_Protocol;
+
+public class Test_Protocol_Reclaim_Send {
+
+	public static void main(String[] args) {
+		
+		String thisPeerId = "este_pc";
+		long spaceToReclaim = 10000; // bytes
+
+		FileManager fm = new FileManager(thisPeerId);
+
+		Map<String, String> fileNames = fm.readFileIdToName();
+		Map<StoreChunkKey, ReplicationValue> chunkStored = fm.readStoreChunkReplicationRegisters();
+
+		M_Socket mc = new M_Socket("224.224.224.224", 15000);
+		M_Socket mdb = new M_Socket("224.224.224.225", 15001);
+		
+		Backup_Protocol bp = new Backup_Protocol(fm, fileNames, chunkStored, mc, mdb, thisPeerId);
+		Reclaim_Protocol rp = new Reclaim_Protocol(fm, fileNames, chunkStored, mc, thisPeerId, bp);
+
+		System.out.println("Reclaiming disk space");
+		
+		rp.reclaimSpace(spaceToReclaim);
+		
+		System.out.println("Reclaimed " + spaceToReclaim + " bytes");
+
+		System.exit(0);
+	}
+}
